975f5e99416373bfb5860079bac5c9ae4d3e0b29
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-28 11:56:27 -0500

    changing RequestMapping to GetMapping

--- src/main/java/com/codeup/controllers/MathController.java
+++ src/main/java/com/codeup/controllers/MathController.java
@@ -1,33 +1,30 @@
 package com.codeup.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MathController {
 
-  @RequestMapping(value = "/add/{numOne}/and/{numTwo}", method = RequestMethod.GET)
+  @GetMapping("/add/{numOne}/and/{numTwo}")
   @ResponseBody
   public String add(@PathVariable int numOne, @PathVariable int numTwo){
     return numOne + " plus " + numTwo + " = " + (numOne + numTwo);
   }
 
-  @RequestMapping(value = "/subtract/{numOne}/from/{numTwo}", method = RequestMethod.GET)
+  @GetMapping(value = "/subtract/{numOne}/from/{numTwo}")
   @ResponseBody
   public String subtract(@PathVariable int numOne, @PathVariable int numTwo) {
     return "Subtracting " + numOne + " from " + numTwo + " = " + (numTwo - numOne);
   }
 
-  @RequestMapping(value = "/multiply/{numOne}/and/{numTwo}", method = RequestMethod.GET)
+  @GetMapping(value = "/multiply/{numOne}/and/{numTwo}")
   @ResponseBody
   public String multiply(@PathVariable int numOne, @PathVariable int numTwo) {
     return numOne + " times " + numTwo + " = " + (numOne * numTwo);
   }
 
-  @RequestMapping(value = "/divide/{numOne}/by/{numTwo}", method = RequestMethod.GET)
+  @GetMapping(value = "/divide/{numOne}/by/{numTwo}")
   @ResponseBody
   public String divide(@PathVariable int numOne, @PathVariable int numTwo) {
     return numOne + " divided by " + numTwo + " = " + (numOne / numTwo);
