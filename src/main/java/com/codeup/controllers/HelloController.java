683cfd803955e533d69209d9192d4650c6814cd2
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-28 11:44:11 -0500

    for the landing page

--- src/main/java/com/codeup/controllers/HelloController.java
+++ src/main/java/com/codeup/controllers/HelloController.java
@@ -1,8 +1,7 @@
 package com.codeup.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -11,5 +10,17 @@ public class HelloController {
   @ResponseBody
   public String hello() {
     return "Hello from spring";
+  }
+
+  @GetMapping("/hello/{name}")
+  @ResponseBody
+  public String sayHello(@PathVariable String name){
+    return "Hello " + name + "!";
+  }
+
+  @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+  @ResponseBody
+  public String addOne(@PathVariable int number) {
+    return number + " plus one is " + (number + 1) + "!";
   }
 }
