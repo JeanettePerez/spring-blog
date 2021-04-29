6046905b3d45514175b55fa4f6ed1ac06463e7ee
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-29 13:47:05 -0500

    join exercise

--- src/main/java/com/codeup/controllers/HomeController.java
+++ src/main/java/com/codeup/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package com.codeup.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,4 +14,11 @@ public class HomeController {
   public String landing() {
     return "This is the landing page";
   }
+
+  @GetMapping("/home")
+  public String welcome() {
+    return "home"; // this is how it refers to the view (html file)
+  }
+
+
 }
