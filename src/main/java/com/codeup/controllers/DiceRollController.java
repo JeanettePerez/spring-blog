9da39880e6e4d7d59a1cd552ad5acee2f193bdd2
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-29 13:45:32 -0500

    logic for dice roll game

---
+++ src/main/java/com/codeup/controllers/DiceRollController.java
@@ -0,0 +1,33 @@
+package com.codeup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class DiceRollController {
+
+  @GetMapping(value = "/roll-dice")
+  public String diceRoll() {
+    return "roll-dice";
+  }
+
+  @RequestMapping(value = "/roll-dice/{guess}", method = RequestMethod.GET)
+  public String diceRollResults(@PathVariable int guess, Model model) {
+    int randomNumberRolled = (int) (Math.random() * ((6 - 1) + 1) + 1);
+    model.addAttribute("guess", "This is your " + guess);
+    model.addAttribute("randomRoll", randomNumberRolled);
+    model.addAttribute("isCorrect", guess == randomNumberRolled);
+
+    if(guess == randomNumberRolled){
+      model.addAttribute("correctRoll", "You rolled correctly");
+      model.addAttribute("correctRoll", "You Win!!!\nYou guessed the number " + guess + " and rolled the number " + randomNumberRolled);
+    }
+    else {
+      model.addAttribute("incorrectRoll", "Pick again");
+      model.addAttribute("correctRoll", "You guessed the number " + guess + " and rolled the number " + randomNumberRolled);
+    }
+    return "roll-dice";
+  }
+
+}
