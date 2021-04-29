c4fe79e9535383cde61f859be21e24a810d2ab28
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-29 17:04:25 -0500

    looking into refactoring the code

--- src/main/java/com/codeup/controllers/DiceRollController.java
+++ src/main/java/com/codeup/controllers/DiceRollController.java
@@ -15,13 +15,14 @@ public class DiceRollController {
   @RequestMapping(value = "/roll-dice/{guess}", method = RequestMethod.GET)
   public String diceRollResults(@PathVariable int guess, Model model) {
     int randomNumberRolled = (int) (Math.random() * ((6 - 1) + 1) + 1);
-    model.addAttribute("guess", "This is your " + guess);
+    model.addAttribute("guess",  guess);
     model.addAttribute("randomRoll", randomNumberRolled);
     model.addAttribute("isCorrect", guess == randomNumberRolled);
 
     if(guess == randomNumberRolled){
       model.addAttribute("correctRoll", "You rolled correctly");
       model.addAttribute("correctRoll", "You Win!!!\nYou guessed the number " + guess + " and rolled the number " + randomNumberRolled);
+
     }
     else {
       model.addAttribute("incorrectRoll", "Pick again");
