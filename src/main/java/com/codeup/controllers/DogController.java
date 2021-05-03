82b367ef8190263f79e9910a21db0231f644651d
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-03 17:48:05 -0500

    getting dog by id

--- src/main/java/com/codeup/controllers/DogController.java
+++ src/main/java/com/codeup/controllers/DogController.java
@@ -4,6 +4,7 @@ import com.codeup.repositories.DogRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -18,6 +19,12 @@ public class DogController {
   @GetMapping ("/dogs")
   public String dogTest(Model model){
     model.addAttribute("dogs",dogDao.findAll());
+    return "doggos";
+  }
+
+  @GetMapping("/dogs/{id}")
+  public String dogByID(@PathVariable long id, Model model) {
+    model.addAttribute("dogs",dogDao.getOne(id));
     return "doggos";
   }
 }
