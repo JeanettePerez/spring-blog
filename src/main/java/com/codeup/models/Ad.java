7bb3bb922cc0845061235b10dc94bf521c87fc14
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-04 14:59:37 -0500

    ad entity for lecture example

--- src/main/java/com/codeup/models/Ad.java
+++ src/main/java/com/codeup/models/Ad.java
@@ -3,7 +3,7 @@ package com.codeup.models;
 import javax.persistence.*;
 
 @Entity
-//@Table(name="ads")
+@Table(name="ads")
 public class Ad {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +17,52 @@ public class Ad {
 
   @Column(nullable = false)
   private int priceInCent;
+
+  public Ad() {
+  }
+
+  public Ad(long id, String title, String description, int priceInCent) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.priceInCent = priceInCent;
+  }
+
+  public Ad(String title, String description, int priceInCent) {
+    this.title = title;
+    this.description = description;
+    this.priceInCent = priceInCent;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public int getPriceInCent() {
+    return priceInCent;
+  }
+
+  public void setPriceInCent(int priceInCent) {
+    this.priceInCent = priceInCent;
+  }
 }
