abb13ed9eefb7dc8f5b823fddd1490da2eadb1e8
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-06 16:31:42 -0500

    adding relationship to user refactoring constructors

--- src/main/java/com/codeup/models/Post.java
+++ src/main/java/com/codeup/models/Post.java
@@ -16,20 +16,31 @@ public class Post {
   @Column(nullable = false, columnDefinition = "TEXT")
   private String body;
 
+  @ManyToOne
+  private User user;
 
-  public Post(long id, String title, String body) {
+  public Post(long id, String title, String body, User user) {
     this.id = id;
     this.title = title;
     this.body = body;
+    this.user = user;
   }
 
-  public Post(String title, String body) {
+  public Post(String title, String body, User user) {
     this.title = title;
     this.body = body;
+    this.user = user;
   }
 
   public Post() {
+  }
 
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
   }
 
   public String getTitle() {
