cb3f803bf291f26f4055fc4d5e8f4c14825aec0d
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-06 16:32:11 -0500

    adding new post seeder to include user id

--- post_seeder.sql
+++ post_seeder.sql
@@ -1,34 +1,10 @@
-insert into posts (title, body) values ('Delicatessen', 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
-
-In congue. Etiam justo. Etiam pretium iaculis justo.
-
-In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.');
-insert into posts (title, body) values ('Sherlock Holmes in Washington', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
-
-Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.
-
-Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.');
-insert into posts (title, body) values ('TerrorVision', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.');
-insert into posts (title, body) values ('Zoom', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
-
-Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
-
-Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.');
-insert into posts (title, body) values ('Telstar: The Joe Meek Story', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
-
-Phasellus in felis. Donec semper sapien a libero. Nam dui.
-
-Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.');
-insert into posts (title, body) values ('Antares', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
-
-Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
-
-Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.');
-insert into posts (title, body) values ('Garden of Words, The (Koto no ha no niwa)', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
-
-Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.');
-insert into posts (title, body) values ('Ticking Clock', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.');
-insert into posts (title, body) values ('Cherry Crush', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.');
-insert into posts (title, body) values ('City of No Limits, The (la ciudad sin límites, En)', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
-
-Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.');
+insert into Posts (title, body, user_id) values ('Lucky Jordan', 'sagittis sapien cum sociis natoque penatibus et magnis dis parturient', 3);
+insert into Posts (title, body, user_id) values ('Starter for 10', 'posuere cubilia curae nulla dapibus dolor vel est donec odio', 2);
+insert into Posts (title, body, user_id) values ('Public Housing', 'sed justo pellentesque viverra pede ac diam cras pellentesque volutpat dui maecenas tristique', 2);
+insert into Posts (title, body, user_id) values ('King of California', 'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis', 1);
+insert into Posts (title, body, user_id) values ('Age of Stupid, The', 'sit amet lobortis sapien sapien non mi integer ac neque duis bibendum morbi', 3);
+insert into Posts (title, body, user_id) values ('Stupids, The', 'eget semper rutrum nulla nunc purus phasellus in felis donec semper sapien a libero', 3);
+insert into Posts (title, body, user_id) values ('Divide, The', 'et eros vestibulum ac est lacinia nisi venenatis tristique fusce congue', 2);
+insert into Posts (title, body, user_id) values ('Amistad', 'lobortis ligula sit amet eleifend pede libero quis orci nullam molestie nibh', 2);
+insert into Posts (title, body, user_id) values ('Dead Man and Being Happy, The (El muerto y ser feliz)', 'posuere metus vitae ipsum aliquam non mauris morbi non lectus', 2);
+insert into Posts (title, body, user_id) values ('Earth Entranced (Terra em Transe)', 'cubilia curae donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin mi', 2);
