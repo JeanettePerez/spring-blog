33c58fae17c625af3465fdf5ac08014cc258cd87
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-07 17:04:13 -0500

    service

---
+++ src/main/java/com/codeup/services/UserDetailsLoader.java
@@ -0,0 +1,30 @@
+package com.codeup.services;
+
+import com.codeup.models.User;
+import com.codeup.models.UserWithRoles;
+import com.codeup.repositories.Users;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+
+  private final Users users;
+
+  public UserDetailsLoader(Users users) {
+    this.users = users;
+  }
+
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = users.findByUsername(username);
+    if (user == null) {
+      throw new UsernameNotFoundException("No user found for " + username);
+    }
+
+    return new UserWithRoles(user);
+  }
+}
