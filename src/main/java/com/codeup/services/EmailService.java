74bb32c7789f035d36b3848aa99f9599ad20b087
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-07 15:49:02 -0500

    email service for when post aare created

--- src/main/java/com/codeup/services/EmailService.java
+++ src/main/java/com/codeup/services/EmailService.java
@@ -1,4 +1,38 @@
 package com.codeup.services;
 
-public class EmailServices {
+import com.codeup.models.Ad;
+import com.codeup.models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+  public final JavaMailSender emailSender;
+
+  @Value("${spring.mail.from}")
+  private String from;
+
+
+  public EmailService(JavaMailSender emailSender) {
+    this.emailSender = emailSender;
+  }
+
+  public void prepareAndSend(Post post, String subject, String body) {
+    SimpleMailMessage msg = new SimpleMailMessage();
+    msg.setFrom(from);
+    msg.setTo(post.getUser().getEmail());
+    msg.setSubject(subject);
+    msg.setText(body);
+    try {
+      this.emailSender.send(msg);
+    }
+    catch (MailException e) {
+      System.err.println(e.getMessage());
+    }
+  }
 }
