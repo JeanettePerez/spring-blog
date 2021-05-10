0b040efa381b5a21b2fe6370643aebf6c2472d64
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-10 14:23:14 -0500

    deleted authorization method

--- src/main/java/com/codeup/controllers/UserController.java
+++ src/main/java/com/codeup/controllers/UserController.java
@@ -1,13 +1,7 @@
 package com.codeup.controllers;
 
 import com.codeup.models.User;
-import com.codeup.models.UserWithRoles;
-import com.codeup.repositories.Users;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import com.codeup.repositories.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,18 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collections;
-
 @Controller
 public class UserController {
 
-  private Users users;
+  private UserRepository userDao;
   private PasswordEncoder passwordEncoder;
 
-  public UserController(Users users, PasswordEncoder passwordEncoder) {
-    this.users = users;
+  public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+    this.userDao = userDao;
     this.passwordEncoder = passwordEncoder;
-
   }
 
   @GetMapping("/sign-up")
@@ -39,19 +30,8 @@ public class UserController {
   public String saveUser(@ModelAttribute User user) {
     String hash = passwordEncoder.encode(user.getPassword());
     user.setPassword(hash);
-    users.save(user);
-    authenticate(user);
+    userDao.save(user);
     return "redirect:/login";
-  }
-
-  private void authenticate(User user) {
-    UserDetails userDetails = new UserWithRoles(user, Collections.emptyList());
-    Authentication auth = new UsernamePasswordAuthenticationToken(
-      userDetails,
-      userDetails.getPassword(),
-      userDetails.getAuthorities());
-    SecurityContext context = SecurityContextHolder.getContext();
-    context.setAuthentication(auth);
   }
 
 
