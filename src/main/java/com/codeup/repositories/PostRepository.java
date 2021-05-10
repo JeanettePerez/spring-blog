c3e25810a6debd407c71d2e0a41c04a6140d062e
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-10 17:34:32 -0500

    adding query to find by user

--- src/main/java/com/codeup/repositories/PostRepository.java
+++ src/main/java/com/codeup/repositories/PostRepository.java
@@ -1,7 +1,11 @@
 package com.codeup.repositories;
 
 import com.codeup.models.Post;
+import com.codeup.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+  List<Post> findAllByUser(User user);
 }
