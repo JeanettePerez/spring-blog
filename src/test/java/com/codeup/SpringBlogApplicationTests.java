b00ef498d8235d7d99afd19cc5b78583e8e4da55
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-11 15:49:23 -0500

    sanity test

--- src/test/java/com/codeup/SpringBlogApplicationTests.java
+++ src/test/java/com/codeup/SpringBlogApplicationTests.java
@@ -3,11 +3,15 @@ package com.codeup;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertNotNull;
+
 @SpringBootTest
 class SpringBlogApplicationTests {
 
   @Test
   void contextLoads() {
   }
+
+
 
 }
